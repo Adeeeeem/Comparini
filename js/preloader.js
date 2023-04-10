79da368c4485d2da4855ce5ee6b3ab3d587eb9d9
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-04-10 23:31:06 +0100

    Add translate option

--- js/preloader.js
+++ js/preloader.js
@@ -16,7 +16,6 @@ $(window).on("load", function()
 		$(document.body).css("--text-color", "#333333");
 		$(document.body).css("--nav-color", "#FFFFFF");
 		$(document.body).css("--nav-text-color", "#046DB5");
-
 	}
 	else
 	{
@@ -25,4 +24,57 @@ $(window).on("load", function()
 		$(document.body).css("--nav-color", "#242526");
 		$(document.body).css("--nav-text-color", "#00A8DE");
 	}
+/*==================================================
+				Language
+==================================================*/
+	if (localStorage.getItem("COMPARINI_LANGUAGE") == null){localStorage.setItem("COMPARINI_LANGUAGE", "english");}
+
+	translate(localStorage.getItem("COMPARINI_LANGUAGE"), "language-tag");
 });
+/*==================================================
+			Language Translate Function
+==================================================*/
+function Translate()
+{
+	// Initialization
+	this.init =  function(attribute, language)
+	{
+		this.attribute = attribute;
+		this.language = language;	
+	}
+	// Translate 
+	this.process = function(){
+		_self = this;
+		var xrhFile = new XMLHttpRequest();
+		// Load content data 
+		xrhFile.open("GET", "languages/" + this.language + ".json", false);
+		xrhFile.onreadystatechange = function ()
+		{
+			if(xrhFile.readyState === 4)
+			{
+				if(xrhFile.status === 200 || xrhFile.status == 0)
+				{
+					var LanguageObject = JSON.parse(xrhFile.responseText);
+					var allDom = document.getElementsByTagName("*");
+					for(var i = 0; i < allDom.length; i++)
+					{
+						var elem = allDom[i];
+						var key = elem.getAttribute(_self.attribute);
+						if(key != null)
+						{
+							elem.innerHTML = LanguageObject[key];
+						}
+					}
+				}
+			}
+		}
+		xrhFile.send();
+	}
+}
+
+function translate(language, tagAttr)
+{
+	var translate = new Translate();
+	translate.init(tagAttr, language);
+	translate.process();
+}
--- js/script.js
+++ js/script.js
@@ -19,3 +19,22 @@ $(function()
 		$(document.body).css("--nav-text-color", "#00A8DE");
 	});
 });
+/* Change Language */
+$(function()
+{
+	$("#english").click(function()
+	{
+		translate("english", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "english");
+	});
+	$("#french").click(function()
+	{
+		translate("french", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "french");
+	});
+	$("#arabic").click(function()
+	{
+		translate("arabic", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "arabic");
+	});
+});
