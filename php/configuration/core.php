54fa4b9a39807e4c530c42c3d8ba29cc589bcd70
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-04-15 01:15:48 +0100

    JWT Customer Configuration

---
+++ php/configuration/core.php
@@ -0,0 +1,14 @@
+<?php
+	/* The Key used to sign and verify tokens */
+	define("JWT_SECRET", "comparini");
+	/* The Algorithm used for signing and verifying tokens */
+	define("JWT_ALGORITHM", "HS256");
+	/* The amount of time (in seconds) that a token is valid for */
+	define("JWT_EXPIRATION_TIME", 3600); // 1 hour
+	/* The issuer of the token (typically your website or application) */
+	define("JWT_ISSUER", "http://Comparini.tn");
+	/* The timezone used for token expiration */
+	date_default_timezone_set("Africa/Tunis");
+	/* Show error reports */
+	error_reporting(E_ALL); 
+?>
