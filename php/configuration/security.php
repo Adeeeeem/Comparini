a3a7f02655daf22db3da6a83a7c2f8afa5f56f4c
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-04-15 13:41:17 +0100

    Remove the filter_var

--- php/configuration/security.php
+++ php/configuration/security.php
@@ -20,9 +20,6 @@
 		// Escape any backslashes to prevent SQL injection attacks
 		$value = str_replace("\\", "\\\\", $value);
 		
-		// Encode special characters to prevent XSS attacks
-		$value = filter_var($value, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH);
-		
 		return $value;
 	}
 ?>
