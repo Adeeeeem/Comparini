643340c217daa9ab5ae226caab4560f912b2b569
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-04-20 00:45:56 +0100

    Make the change theme and language global

--- js/preloader.js
+++ js/preloader.js
@@ -32,6 +32,52 @@ $(window).on("load", function()
 	translate(localStorage.getItem("COMPARINI_LANGUAGE"), "language-tag");
 });
 /*==================================================
+			Change Theme
+==================================================*/
+/* Turn ON and OFF Dark Theme */
+$(function()
+{
+	$("#light_theme").click(function()
+	{
+		localStorage.setItem("COMPARINI_THEME", "LIGHT");
+		$(document.body).css("--body-color", "#F8F8F8");
+		$(document.body).css("--text-color", "#333333");
+		$(document.body).css("--nav-color", "#FFFFFF");
+		$(document.body).css("--nav-text-color", "#046DB5");
+	});
+
+	$("#dark_theme").click(function()
+	{
+		localStorage.setItem("COMPARINI_THEME", "DARK");
+		$(document.body).css("--body-color", "#18191A");
+		$(document.body).css("--text-color", "#FFFFFF");
+		$(document.body).css("--nav-color", "#242526");
+		$(document.body).css("--nav-text-color", "#00A8DE");
+	});
+});
+/*==================================================
+			Change Language
+==================================================*/
+/* Change Language */
+$(function()
+{
+	$("#english").click(function()
+	{
+		translate("english", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "english");
+	});
+	$("#french").click(function()
+	{
+		translate("french", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "french");
+	});
+	$("#arabic").click(function()
+	{
+		translate("arabic", "language-tag");
+		localStorage.setItem("COMPARINI_LANGUAGE", "arabic");
+	});
+});
+/*==================================================
 			Language Translate Function
 ==================================================*/
 function Translate()
@@ -72,7 +118,6 @@ function Translate()
 		xrhFile.send();
 	}
 }
-
 function translate(language, tagAttr)
 {
 	var translate = new Translate();
