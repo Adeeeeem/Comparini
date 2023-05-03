33745f8d2fcf6a94160c09d808f31a5750506c05
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-05-03 19:05:47 +0100

    Add the rest of the database

--- Comparini.sql
+++ Comparini.sql
@@ -30,32 +30,32 @@ USE `Comparini`;
 --
 
 DROP TABLE IF EXISTS `Admin`;
-CREATE TABLE IF NOT EXISTS `Admin` (
-	`id` int(2) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Admin`
+(
+	`id` int(2) NOT NULL AUTO_INCREMENT,
 	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
 	`username` varchar(25) NOT NULL,
 	`password` varchar(255) NOT NULL,
 	`first_name` varchar(50) NOT NULL,
 	`last_name` varchar(50) NOT NULL,
-	`email` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- RELATIONSHIPS FOR TABLE `Admin`:
---
+	`email` varchar(100) DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `username` (`username`),
+	UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Truncate table before insert `admin`
 --
 
 TRUNCATE TABLE `Admin`;
+
 --
 -- Dumping data for table `Admin`
 --
 
 INSERT INTO `Admin` (`id`, `is_enabled`, `username`, `password`, `first_name`, `last_name`, `email`) VALUES
 (1, 1, 'admin', '$2y$10$PG7XDyG.umXMdG36sutkG.IZI6XW8jnciKtOMhD4/2Eov4WlNeiIu', 'Mohamed Adem', 'Ben Moussa', 'MohamedAdemBenMoussa@Gmail.com');
-COMMIT;
 
 -- --------------------------------------------------------
 
@@ -64,22 +64,22 @@ COMMIT;
 --
 
 DROP TABLE IF EXISTS `Category`;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `is_enabled` tinyint(1) NOT NULL DEFAULT 1,
-  `label` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- RELATIONSHIPS FOR TABLE `Category`:
---
+CREATE TABLE IF NOT EXISTS `Category`
+(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
+	`label` varchar(255) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `label` (`label`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Truncate table before insert `Category`
 --
 
 TRUNCATE TABLE `Category`;
+
 --
 -- Dumping data for table `Category`
 --
@@ -101,40 +101,151 @@ INSERT INTO `Category` (`id`, `is_enabled`, `label`, `name`) VALUES
 (14, 1, 'health_foods_supplements', 'Health Foods and Supplements'),
 (15, 1, 'electronics', 'Electronics');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+CREATE TABLE IF NOT EXISTS `Product`
+(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
+	`name` varchar(255) NOT NULL,
+	`description` varchar(255) DEFAULT NULL,
+	`image` varchar(255) NOT NULL,
+	`viewed` int(11) NOT NULL DEFAULT 1,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `Product`
+--
+
+TRUNCATE TABLE `Product`;
+
+--
+-- Dumping data for table `Product`
+--
+
+-- INSERT
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product_Provider`
+--
+
+DROP TABLE IF EXISTS `Product_Provider`;
+CREATE TABLE IF NOT EXISTS `Product_Provider`
+(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
+	`price` decimal(10,0) NOT NULL,
+	`link` varchar(255) DEFAULT NULL,
+	`product_id` int(11) NOT NULL,
+	`provider_id` int(11) NOT NULL,
+	`sub_category_id` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `fk_product` (`product_id`),
+	KEY `fk_provider` (`provider_id`),
+	KEY `fk_sub_category` (`sub_category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `Product_Provider`
+--
+
+TRUNCATE TABLE `Product_Provider`;
+
+--
+-- Dumping data for table `Product_Provider`
+--
+
+-- INSERT
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Provider`
+--
+
+DROP TABLE IF EXISTS `Provider`;
+CREATE TABLE IF NOT EXISTS `Provider`
+(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
+	`name` varchar(255) NOT NULL,
+	`adresse` varchar(255) NOT NULL,
+	`image` varchar(255) NOT NULL,
+	`link` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `Provider`
+--
+
+TRUNCATE TABLE `Provider`;
+
+--
+-- Dumping data for table `Provider`
+--
+
+-- INSERT
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Sub_Category`
+--
+
+DROP TABLE IF EXISTS `Sub_Category`;
+CREATE TABLE IF NOT EXISTS `Sub_Category`
+(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`is_enabled` tinyint(1) NOT NULL DEFAULT 1,
+	`name` varchar(255) NOT NULL,
+	`category_id` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `fk_category` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `Sub_Category`
+--
+
+TRUNCATE TABLE `Sub_Category`;
+
+--
+-- Dumping data for table `Sub_Category`
+--
+
+-- INSERT
+
+-- --------------------------------------------------------
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `Admin`
---
-ALTER TABLE `Admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `Category`
---
-ALTER TABLE `Category`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `label` (`label`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Constraints for table `Product_Provider`
 --
 
---
--- AUTO_INCREMENT for table `Admin`
---
-ALTER TABLE `Admin`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `product_provider`
+	ADD CONSTRAINT `fk_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT `fk_provider` FOREIGN KEY (`provider_id`) REFERENCES `provider` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	ADD CONSTRAINT `fk_sub_category` FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- AUTO_INCREMENT for table `Category`
+-- Constraints for table `sub_category`
 --
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+ALTER TABLE `sub_category`
+	ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
